de8ac3497e959efdcdc2a04ff51cddf29907f1ab
y-nut <ttkaae@gmail.com>
2019-03-28 17:14:55 -0300

    back to normal

--- src/android/CameraPreview.java
+++ src/android/CameraPreview.java
@@ -284,16 +284,16 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
         }
         //display camera bellow the webview
         if(toBack){
-/*
+
           webView.getView().setBackgroundColor(0x00000000);
           webViewParent = webView.getView().getParent();
            ((ViewGroup)webView.getView()).bringToFront();
-*/
 
+/*
           ((ViewGroup)webView.getView().getParent().getParent()).setBackgroundColor(0x00000000);
           webViewParent = webView.getView().getParent().getParent();
           ((ViewGroup)webView.getView().getParent().getParent()).bringToFront();
-
+*/
         }else{
 
           //set camera back to front
